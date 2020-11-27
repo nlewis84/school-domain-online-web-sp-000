0f0d587e4b515ca82bd0bb84ceeb201681791cb1
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-27 23:43:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require 'pry'
+
+class School
+  
+  attr_accessor :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|roster, grade| roster[grade] = []}
+  end
+  
+  def add_student(student_name, grade)
+    roster[grade] += [student_name]
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort ## I am only sorting grades...i need to sort names
+    sorted = roster.sort
+    roster = Hash[sorted.map {|key, value| [key, value]}]
+    binding.pry
+  end
+end
